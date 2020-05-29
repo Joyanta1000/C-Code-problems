c1ffc2b7f8e10672f088d251abd6e4e353d34936
Joyanta Dutta <58521830+Joyanta1000@users.noreply.github.com>
2020-05-29 23:34:54 +0600

    translation

---
+++ translation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	string a;
+	string b;
+	string c;
+	
+	cin>>a;
+	
+	cin>>b;
+	
+	reverse(b.begin(),b.end());
+	
+	if(b==a)
+	
+	{
+		cout<<"YES";
+	}
+	
+	else
+	
+	{
+		cout<<"NO";
+	}
+	
+}
